f17826deb9a23f12e9ada69f3529df59db067423
Tony Hall <tony.hall@nih.gov>
2016-06-06 09:41:42 -0400

    adding another image size, using galleria plugin

--- restifo/arrayGen.py
+++ restifo/arrayGen.py
@@ -1,44 +1,12 @@
 import os, sys, json
 from PIL import Image
 
-dirs_pw = os.listdir( "images/pw_heatmaps/" )
-dirs_core = os.listdir("images/core_heatmaps/")
-
-arr_pw = list()
-arr_core = list()
-
-tn_size = 591, 591
+bg_size = 600,600
+tn_size = 296, 296
 
 try:
-    if not os.path.exists('images/core_heatmaps/thumbs/'):
-        os.makedirs('images/core_heatmaps/thumbs/')
-    elif not os.path.exists('images/pw_heatmaps/thumbs/'):
-        os.makedirs('images/pw_heatmaps/thumbs/')
-
-    for file in dirs_pw:
-        if file.endswith(".png") is False:
-            continue
-
-        print("working on file '" + file + "'")
-        img = Image.open("images/pw_heatmaps/" + file)
-        img.thumbnail(tn_size)
-        img.save("images/pw_heatmaps/thumbs/" + file)
-
-        arr_pw.append(["images/pw_heatmaps/thumbs/" + file, "images/pw_heatmaps/" + file])
-
-    for file in dirs_core:
-        if file.endswith(".png") is False:
-            continue
-
-        print("working on file '" + file + "'")
-        img = Image.open("images/core_heatmaps/" + file)
-        img.thumbnail(tn_size)
-        img.save("images/core_heatmaps/thumbs/" + file)
-
-        arr_core.append([ "images/core_heatmaps/thumbs/" + file, "images/core_heatmaps/" + file ])
-
-    arr_pw.sort()
-    arr_core.sort()
+    arr_pw = processImages('pw_heatmaps')
+    arr_core = processImages('core_heatmaps')
 
     with open("images.json", 'wb') as outfile:
         data = {}
@@ -51,5 +19,46 @@ except Exception as e:
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print("EXCEPTION------------------------------", exc_type, fname, exc_tb.tb_lineno)
 finally:
-    print "done...."
+    print "done....<Press enter key to exit>"
     raw_input()
+
+
+def processImages(dirName):
+    try:
+        print("In process images")
+
+        filesList = list()
+        imageDir = os.listdir("images/" + dirName + "/" )
+
+        if not os.path.exists('images/' + dirName + '/big/'):
+            os.makedirs('images/' + dirName + '/big/')
+
+        if not os.path.exists('images/' + dirName + '/thumbs/'):
+            os.makedirs('images/' + dirName + '/thumbs/')
+
+        for file in imageDir:
+            if file.endswith(".png") is False:
+                continue
+
+            print("working on file '" + file + "'")
+            img_tn = Image.open('images/' + dirName + '/' + file)
+            img_tn.thumbnail(tn_size)
+            img_tn.save('images/' + dirName + '/thumbs/' + file, quality=70)
+
+            img_big = Image.open('images/' + dirName + '/' + file)
+            img_big.thumbnail(bg_size)
+            img_big.save('images/' + dirName + '/big/' + file, quality=70)
+
+            filesList.append([
+                    'images/' + dirName + '/thumbs/' + file,
+                    'images/' + dirName + '/' + file,
+                    'images/' + dirName + '/big/' + file ])
+
+            filesList.sort()
+
+    except Exception as e:
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("EXCEPTION------------------------------", exc_type, fname, exc_tb.tb_lineno)
+    finally:
+        return filesList
--- restifo/ccrBioCore.js
+++ restifo/ccrBioCore.js
@@ -31,7 +31,22 @@ function getImages() {
     }).done(function (data) {
         buildGalleryRows(data.core, "#coreGallery", "core");
         buildGalleryRows(data.pw, "#pathwayGallery", "pathway");
-    }).fail(function(jqXHR, textStatus){
+        if (!Galleria) {
+            $(".prev, .next ").on("click", function () {
+                var activeGallery = $("#coreGallery, #pathwayGallery").has(".show");
+                var currentImg = activeGallery.find(".imagePreview img")[0];
+                var tn = $("img[name='" + currentImg.name + "']").not(currentImg);
+
+                var imgCount = activeGallery.find("img.img-thumbnail").length;
+                var imgIndex = activeGallery.find("img.img-thumbnail").index(tn);
+
+                if ($(this).hasClass("prev") && imgIndex > 0)
+                    $(activeGallery.find("a.expand")[imgIndex - 1]).trigger("click");
+                if ($(this).hasClass("next") && imgIndex != imgCount)
+                    $(activeGallery.find("a.expand")[imgIndex + 1]).trigger("click");
+            });
+        }
+    }).fail(function (jqXHR, textStatus) {
         $("#coreGallery, #pathwayGallery").append("<div class='alert alert-warning'>Cannot retrieve images. Try again later &hellip;</div>");
 
     });
@@ -41,51 +56,95 @@ function buildGalleryRows(items, containerElement, imgGroupName) {
     if (items.length <= 0) {
         $(containerElement).append("<div class='alert alert-warning'>Cannot retrieve images. Try again later.</div>");
     } else {
-        $(containerElement).append("<div id='" + imgGroupName + "Preview' class='imagePreview'></div><div class='row'></div>");
+
+        var imagesArray = [];
 
         for (var i = 0; i < items.length; i++) {
-            if (i > 0 && (i % 6 === 0)) {
-                $(containerElement).append("<div class='row'></div>");
+            var imageObject = {
+                thumb: null,
+                image: null,
+                big: null,
+                title: null,
+                description: null
+            };
+
+            imageObject.thumb = items[i][0];
+            imageObject.big = items[i][1];
+            imageObject.image = items[i][2];
+            imageObject.title = imageObject.image.split(".")[1] + " " + imgGroupName;
+            imageObject.description = imageObject.title.replace(" ", "_").toLowerCase();
+
+            imagesArray.push(imageObject);
+        }
+
+        if (imagesArray.length > 0) {
+            if (Galleria) {
+                Galleria.loadTheme('https://cdnjs.cloudflare.com/ajax/libs/galleria/1.4.2/themes/classic/galleria.classic.js');
+                //Galleria.loadTheme('ccrbcTheme/galleria.ccrbcTheme.js');
+                Galleria.configure({
+                    wait: true,
+                    width: '100%',
+                    height: '1.5',
+                    responsive: true,
+                    dummy: "../images/loader.gif",
+                    thumbQuality: false,
+                    idleMode: false,
+                    //        imagecrop: 'landscape',
+                    lightbox: true,
+                    opacityOverlay: 0.65,
+                    preload: 10
+                });
+
+                Galleria.run(containerElement, {
+                    dataSource: imagesArray
+                });
+            } else {
+                // fallback if galleria does not load
+                $(containerElement).append("<div class='row'><div id='" + imgGroupName + "Preview' class='imagePreview'></div></div><div class='row'><button type='button' class='prev col-sm-2 pull-left'><span class='glyphicon glyphicon-arrow-left'></span></button><span class='imgIndex'></span><button type='button' class='next col-sm-2 pull-right'><span class='glyphicon glyphicon-arrow-right'></span></button></div><div class='row'></div>");
+
+                for (var i = 0; i < items.length; i++) {
+                    if (i > 0 && (i % 6 === 0)) {
+                        $(containerElement).append("<div class='row'></div>");
+                    }
+
+                    var rowIndex = $(containerElement + " .row").length - 1;
+                    var galleryRow = $(containerElement + " .row")[rowIndex];
+
+                    var tn_filename = items[i][0];
+                    var fs_filename = items[i][1];
+
+                    var imgTitle = tn_filename.split(".")[1] + " " + imgGroupName;
+                    var imgName = imgTitle.replace(" ", "_").toLowerCase();
+
+                    var img = new Image();
+                    img.src = tn_filename;
+
+                    $(galleryRow).append("<div class='col-sm-4 col-md-2'>" +
+                        tn_filename.split(".")[1] + "<a class='expand' href='#'>" +
+                        "<img class='img-thumbnail' name='" + imgName + "' src='" +
+                        tn_filename + "' data-zoom-image='" + fs_filename + "' alt='" +
+                        imgTitle + " heatmap' />" + "<span class='glyphicon glyphicon-fullscreen'>" + "</span></a></div>");
+
+                    $(containerElement).find("a.expand").on("click", function () {
+                        var img = $(this).find("img").clone();
+
+                        $("#" + imgGroupName + "Preview").html("").addClass("show");
+                        $("#" + imgGroupName + "Preview").html(img);
+
+                        document.getElementsByClassName("imagePreview")[0].scrollIntoView();
+
+                        if (isMobileBrowser()) {
+                            $("#" + imgGroupName + "Preview img").attr("src", $(this).find("img").attr("data-zoom-image"));
+                        } else {
+                            $("#" + imgGroupName + "Preview img").elevateZoom();
+                        }
+                    });
+                }
             }
-
-            var rowIndex = $(containerElement + " .row").length - 1;
-            var galleryRow = $(containerElement + " .row")[rowIndex];
-
-            var tn_filename = items[i][0];
-            var fs_filename = items[i][1];
-
-            var imgTitle = tn_filename.split(".")[1] + " " + imgGroupName;
-            var imgName = imgTitle.replace(" ", "_").toLowerCase();
-
-            var img = new Image();
-            img.src = tn_filename;
-
-            $(galleryRow).append("<div class='col-sm-4 col-md-2'>" +
-                tn_filename.split(".")[1] + "<a class='expand' href='#'>" +
-                "<img class='img-thumbnail' name='" + imgName + "' src='" +
-                tn_filename + "' data-zoom-image='" + fs_filename + "' alt='" +
-                imgTitle + " heatmap' />" + "<span class='glyphicon glyphicon-fullscreen'>" + "</span></a></div>");
-
-            $(containerElement).find("a.expand").on("click", function () {
-                var img = $(this).find("img").clone();
-
-                $("#" + imgGroupName + "Preview").html("").addClass("show");
-                $("#" + imgGroupName + "Preview").html(img);
-
-                document.getElementsByClassName("imagePreview")[0].scrollIntoView();
-
-                if (isMobileBrowser()) {
-                    $("#" + imgGroupName + "Preview img").attr("src", $(this).find("img").attr("data-zoom-image") );
-                }
-                else {
-                    $("#" + imgGroupName + "Preview img").elevateZoom();
-                }
-            });
         }
 
         $(containerElement).prepend("<a href='images/" + imgGroupName.capitalize() +
             "_heatmaps.zip' class='download btn btn-default'><span class='glyphicon glyphicon-download'></span> Download  " + imgGroupName.capitalize() + " Heatmaps</a>");
-
     }
 }
 
@@ -95,13 +154,6 @@ function isMobileBrowser() {
     } else {
         return false;
     }
-}
-
-function callbackFunction(_callback, params) {
-    if (params)
-        _callback(params);
-    else
-        _callback();
 }
 
 String.prototype.capitalize = function () {
